c17c95cf447936c292670c93cb9d8b7bc21c4b2c
Jesper <Mollezeta@gmail.com>
2018-04-26 14:46:52 +0200

    Search function
    not done

---
+++ step-definitions/search-function.js
@@ -0,0 +1,21 @@
+ let SearchBeverage = require("../search-function");
+
+module.exports = function() {
+  let searchBeverage = new SearchBeverage();
+
+  this.Given(/^that there is a beverage name to search for$/, function (callback) {
+    //Search for beer
+    searchBeverage("beer");
+    callback();
+  });
+
+  this.When(/^search is executed$/, function (callback) {
+    
+    callback();
+  });
+
+  this.Then(/^an array containing all the beverages with the same name willshow up$/, function (callback) {
+
+    callback();
+  });
+}
