f4a969e3011e51fe37a39e9caeeccd7890ad189e
Tryhardmatte <tryhardmatte@gmail.com>
2018-04-26 10:04:37 +0200

    remove all form cart
    Done now for real this time

--- step-definitions/remove-all-from-shopping-cart.js
+++ step-definitions/remove-all-from-shopping-cart.js
@@ -1,12 +1,20 @@
+//let Product = require("../product.js");
 let ShoppingCart = require("../shopping-cart.js");
+let theApp = require("../app.js");
 
 module.exports = function(){
 	//A list of items that we have bought.
 	//change later
-	let thingsToBuy = ShoppingCart.thingsToBuy;
+	     let aShoppingCart;
+       
        this.Given(/^that a person have a shopping cart with items in it$/, function (callback) {
        	 //Adding some fake items to the list to test this
          //fake items
+         aShoppingCart = new ShoppingCart();
+         aShoppingCart.add(theApp.products[500], 5);
+         aShoppingCart.add(theApp.products[1200], 10);
+         console.log(theApp.products[500]);
+         console.log(theApp.products[1200]);
          callback();
        });
 
@@ -14,17 +22,16 @@ module.exports = function(){
        	//When a person clicks the reset buttom then the removeAllItems will be triggered
        	// Use jqueary to se when to button gets clicked
         // $(".reset-button").click(ShoppingCart.removeAllItems);
-        ShoppingCart.removeAllItems;
+        aShoppingCart.removeAllItems();
          callback();
        });
        
        this.Then(/^the shopping cart will be reset$/, function (callback) {
        	//Check if the list includes any items and if it does throw a new error
-         if(thingsToBuy = []){
-
-         } else {
+         /*if(aShoppingCart.thingsToBuy.length > 0){
          	throw new Error("The to buy list still have items in it");
-         }
+         }*/
+         assert.deepEqual(aShoppingCart.thingsToBuy, [], "My error: The shoppinglist is not empty!");
          callback();
        });
 }
