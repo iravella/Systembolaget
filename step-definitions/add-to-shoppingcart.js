f609a810ffe75de48a67d1bc521e490316b6fb55
sandhya <ksandhya22@gmail.com>
2018-04-26 15:52:06 +0200

    add-to-shoppingcart.js, add-to-shoppingcart.feature,

--- step-definitions/add-to-shoppingcart.js
+++ step-definitions/add-to-shoppingcart.js
@@ -1,49 +1,54 @@
-// let Person = require('../person.js');
-// let myApp = require('../app.js');
+let Person = require('../person.js');
+let myApp = require('../app.js');
 
 
 
-// module.exports = function()
+module.exports = function()
 
-// {
-//   let aPerson, category;
+{
+  let aPerson;
+  let productToAdd;
+  let category;
 
-//   this.Given(/^the person is a registered user$/, function(callback) 
-//   {
-//     // Write code here that turns the phrase above into concrete actions
-//     callback();
-//   });
-
-
-//   this.Given(/^the user is viewing a certain product$/, function(callback) 
-//   {
-//     // Write code here that turns the phrase above into concrete actions
-//     callback();
-//   });
-
-
-
-//   this.Given(/^the quantity has been set to (\d+)$/, function(arg1, callback) 
-//   {
-//     // Write code here that turns the phrase above into concrete actions
-//     callback();
-//   });
+  this.Given(/^the user is a registered user$/, function(callback) 
+  {
+    // For a user to be registrered the application will require
+    // a first name, last name, birthdate and stree address, zipcode, city
+    aPerson = new Person('Anna', 'Andersson', '19950901', 'Kyrkogatan 21', '22222', 'Lund');
+    // If we are really grumpy and there is no other feature testing this
+    // assert that all the data used to create the person "sticks" with thte person
+    //assert(aPerson.firstName === 'Anna', "The firstName of the person is different from the one used to create the person.");
+    //assert(aPerson.lastName === 'Andersson', "The firstName of the person is different from the one used to create the person.");
+    // ....
+    callback();
+  });
 
 
-
-//   this.When(/^the user clicks "([^"]*)"$/, function(arg1, callback) 
-//   {
-//     // Write code here that turns the phrase above into concrete actions
-//     callback();
-//   });
-
-
-//   this.Then(/^the product with the quantity of (\d+) should be added to the user's shopping cart$/,
-//    function(arg1, callback) {
-//     // Write code here that turns the phrase above into concrete actions
-//     callback();
-//   });
+  this.When(/^the user is trying to add a 3 bottles of Captain Morgan to his\/her shopping card$/, function(callback) 
+  {
+    for(let product of myApp.products){
+    	if(product.name === "Captain Morgan"){ 
+    		productToAdd = product;
+    		break;
+    	}
+    }
+    aPerson.shoppingCart.add(productToAdd, 3);
+    callback();
+  });
 
 
 
-// }
+  this.Then(/^3 bottles of Captain Morgan should be to the shopping cart$/, function(callback) 
+  {
+  	let items = aPerson.shoppingCart.getCartItems(), ok = false;
+  	for(let item of items){
+  		if(item.product === productToAdd && item.quantity === 3){
+  			ok = true;
+  			break;
+  		}
+  	}
+  	assert(ok, "Didn't find the added 3 bottles of Captain Morgan in the shopping cart.");
+    callback();
+  });
+
+}
