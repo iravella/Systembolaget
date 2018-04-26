74b376c6a77e1d4f7bb6659ea025f268f37d1466
sandhya <ksandhya22@gmail.com>
2018-04-26 10:58:34 +0200

    add-to-shoppingcart.js, add-to-shoppingcart.feature

---
+++ step-definitions/add-to-shoppingcart.js
@@ -0,0 +1,49 @@
+// let Person = require('../person.js');
+// let myApp = require('../app.js');
+
+
+
+// module.exports = function()
+
+// {
+//   let aPerson, category;
+
+//   this.Given(/^the person is a registered user$/, function(callback) 
+//   {
+//     // Write code here that turns the phrase above into concrete actions
+//     callback();
+//   });
+
+
+//   this.Given(/^the user is viewing a certain product$/, function(callback) 
+//   {
+//     // Write code here that turns the phrase above into concrete actions
+//     callback();
+//   });
+
+
+
+//   this.Given(/^the quantity has been set to (\d+)$/, function(arg1, callback) 
+//   {
+//     // Write code here that turns the phrase above into concrete actions
+//     callback();
+//   });
+
+
+
+//   this.When(/^the user clicks "([^"]*)"$/, function(arg1, callback) 
+//   {
+//     // Write code here that turns the phrase above into concrete actions
+//     callback();
+//   });
+
+
+//   this.Then(/^the product with the quantity of (\d+) should be added to the user's shopping cart$/,
+//    function(arg1, callback) {
+//     // Write code here that turns the phrase above into concrete actions
+//     callback();
+//   });
+
+
+
+// }
