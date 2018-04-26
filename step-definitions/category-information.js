7f88254caebc483f8ee75e4ad2cc53046f332622
swapna <swapnasudha.gandhi@gmail.com>
2018-04-26 15:58:11 +0200

    change to add-shoppingcart

--- step-definitions/category-information.js
+++ step-definitions/category-information.js
@@ -2,6 +2,12 @@
  let myApp = require('../app.js');
 
  module.exports = function() {
+
+ 	this.Given(/^that the person is a registered user$/, function (callback) {
+         // Write code here that turns the phrase above into concrete actions
+         callback();
+       });
+
    this.When(/^the user clicks on products\/certain categories$/, function(callback) {
      // Write code here that turns the phrase above into concrete actions
      callback();
