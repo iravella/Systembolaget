857a7aba1c61750f9a785655548ddaec2606d298
sandhya <ksandhya22@gmail.com>
2018-04-27 15:29:27 +0200

    category.information.js, add-to-shoppingcart.js

--- step-definitions/add-to-shoppingcart.js
+++ step-definitions/add-to-shoppingcart.js
@@ -14,11 +14,10 @@ module.exports = function()
   {
     // For a user to be registrered the application will require
     // a first name, last name, birthdate and street address, zipcode, city
-    aPerson = new Person('Anna', 'Andersson', '19950901', 'Kyrkogatan 21', '22222', 'Lund');
+    aPerson = new Person('Anna Andersson', '19950901', 'Kyrkogatan 21', '22222', 'Lund');
     //If we are really grumpy and there is no other feature testing this
     //assert that all the data used to create the person "sticks" with thte person
-    assert(aPerson.firstName === 'Anna', "The firstName of the person is different from the one used to create the person.");
-    assert(aPerson.lastName === 'Andersson', "The lastName of the person is different from the one used to create the person.");
+    assert(aPerson.fullName === 'Anna Andersson', "The firstName of the person is different from the one used to create the person.");
     assert(aPerson.dateOfBirth === '19950901', "The dateOfBirth of the person is different from the one used to create the person.");
     assert(aPerson.addressStreet === 'Kyrkogatan 21', "The addressStreet of the person is different from the one used to create the person.");    
     assert(aPerson.addressZipCode === '22222', "The addressZipCode of the person is different from the one used to create the person.");
