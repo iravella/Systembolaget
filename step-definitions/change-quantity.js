259fe62bbb173382f1a98cd7142698b3043a211b
sandhya <ksandhya22@gmail.com>
2018-04-30 11:46:57 +0200

    changes has done at change-quantity.js

--- step-definitions/change-quantity.js
+++ step-definitions/change-quantity.js
@@ -1,20 +1,32 @@
 let Person = require('../person.js');
 let myApp = require('../app.js');
+let ShoppingCart = require('../shopping-cart.js');
 
 module.exports = function() {
 
-  this.Given(/^that user has already added a product in their shopping cart$/, function(callback) {
-    // Write code here that turns the phrase above into concrete actions
+  let aShoppingCart, aProduct1, aProduct2;
+
+  this.Given(/^that user has already added a product in their shopping cart$/, function(callback) 
+  {
+    // Creating products and add to the shopping cart
+     aProduct1 = myApp.products[300];
+     aProduct2 = myApp.products[200];
+     aShoppingCart = new ShoppingCart();
+     aShoppingCart.add(aProduct1, 3);
+     aShoppingCart.add(aProduct2, 5);
+     callback();
+  });
+
+
+   this.When(/^the user changes the quantity of the beverage in the cart$/, function (callback) 
+  {
+    // change the quantity of aProduct1(10) insted of aProduct1(3)
     callback();
   });
 
-  this.When(/^the user changes the quantity of the beverage in the cart$/, function(callback) {
-    // Write code here that turns the phrase above into concrete actions
-    callback();
-  });
 
-  this.Then(/^the quantity should be changed in the shopping cart$/, function(callback) {
-    // Write code here that turns the phrase above into concrete actions
-    callback();
-  });
+  // this.Then(/^the quantity should be changed in the shopping cart$/, function (callback) {
+  //   // Write code here that turns the phrase above into concrete actions
+  //   callback(null, 'pending');
+  // });
 }
