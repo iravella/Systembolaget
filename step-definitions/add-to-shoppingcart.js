462fc170cff6bdf5703cef2e45448a8b9b86012f
sandhya <ksandhya22@gmail.com>
2018-05-17 14:13:25 +0200

    changes has done at add-to-shoppingcart.js
    Sandhya and Tabbias pair programming

--- step-definitions/add-to-shoppingcart.js
+++ step-definitions/add-to-shoppingcart.js
@@ -1,5 +1,6 @@
 let Person = require('../person.js');
 let myApp = require('../app.js');
+let ShoppingCart = require('../shopping-cart.js');
 
 
 
@@ -8,7 +9,7 @@ module.exports = function()
 {
   let aPerson;
   let productToAdd;
-  let category, productToAdd1, productToAdd2, productToAdd3, items ;
+  let category, productToAdd1, productToAdd2, productToAdd3, items, emptycart;
 
   this.Given(/^the user is a registered user$/, function(callback) {
     // For a user to be registrered the application will require
@@ -26,15 +27,19 @@ module.exports = function()
 
 
   this.When(/^the user is trying to add  (\d+) bottles of Captain Morgan to his\/her shopping cart$/, function(CaptianQty, callback) {
+
+    //console.warn('_______', aPerson.shoppingCart.thingsToBuy);
+
+     //aPerson.shoppingCart.removeAllItems();
     for (let product of myApp.products) {
-      if (product.namn === "Captain Morgan") 
-      {
+      if (product.namn === "Captain Morgan") {
         productToAdd = product;
         break;
       }
     }
-    console.warn(CaptianQty);
+
     aPerson.shoppingCart.add(productToAdd, CaptianQty);
+    //console.warn(aPerson.shoppingCart);
     callback();
   });
 
@@ -43,13 +48,13 @@ module.exports = function()
     let items = aPerson.shoppingCart.getCartItems();
     ok = false;
     for (let item of items) {
-      if (items[0].product === productToAdd && items[0].quantity === CaptianQty) {
+      if (items[0].product === productToAdd && items[0].quantity == CaptianQty) {
         ok = true;
-        
+
       }
     }
     assert(ok, "Didn't find the added 3 bottles of Captain Morgan in the shopping cart.");
-    //console.warn('_______', aPerson.shoppingCart.thingsToBuy[0]);
+    //console.warn('_______', aPerson.shoppingCart.thingsToBuy);
     callback();
   });
 
@@ -58,6 +63,12 @@ module.exports = function()
 
   this.Given(/^the user's shopping cart is empty$/, function(callback) {
     // Write code here that turns the phrase above into concrete actions
+
+    //console.warn("_________________", aPerson.shoppingCart.thingsToBuy);
+    aPerson.shoppingCart.removeAllItems();
+
+    //console.warn("**********", aPerson.shoppingCart.thingsToBuy);
+
     callback();
   });
 
@@ -65,7 +76,7 @@ module.exports = function()
   this.When(/^the user is trying to add (\d+) bottles of Renat to his\/her shopping cart$/,
     function(RenatQty, callback) {
       // Write code here that turns the phrase above into concrete actions
-      
+
       for (let product of myApp.products) {
         if (product.namn === "Renat") {
           productToAdd1 = product;
@@ -73,7 +84,7 @@ module.exports = function()
         }
       }
       aPerson.shoppingCart.add(productToAdd1, RenatQty);
-      console.warn('******', aPerson.shoppingCart.thingsToBuy[0]);
+      //console.warn('******', aPerson.shoppingCart.thingsToBuy[0]);
 
       callback();
     });
@@ -81,7 +92,7 @@ module.exports = function()
 
   this.When(/^the user is trying to add (\d+) bottles of Canella to his\/her shopping cart$/, function(CanellaQty, callback) {
     // Write code here that turns the phrase above into concrete actions
-    
+
     for (let product of myApp.products) {
       if (product.namn === "Canella") {
         productToAdd2 = product;
@@ -89,7 +100,7 @@ module.exports = function()
       }
     }
     aPerson.shoppingCart.add(productToAdd2, CanellaQty);
-    console.warn(aPerson.shoppingCart);
+    //console.warn(aPerson.shoppingCart);
 
     callback();
   });
@@ -97,7 +108,7 @@ module.exports = function()
 
   this.When(/^the user is trying to add (\d+) bottles of Purcari to his\/her shopping cart$/, function(PurcariQty, callback) {
     // Write code here that turns the phrase above into concrete actions
-    
+
     for (let product of myApp.products) {
       if (product.namn === "Purcari") {
         productToAdd3 = product;
@@ -105,114 +116,163 @@ module.exports = function()
       }
     }
     aPerson.shoppingCart.add(productToAdd3, PurcariQty);
-    console.warn(aPerson.shoppingCart);
+
+    //console.warn('******', aPerson.shoppingCart.thingsToBuy);
+
     callback();
   });
 
 
 
-       
-  this.Then(/^(\d+) bottles of Captain Morgan, (\d+) bottles of Canella and (\d+) bottle of Purcari beverages should be add to his\/her shopping cart$/, function(CaptianQty, CanellaQty, PurcariQty, callback) {
-      
-      
-          // Write code here that turns the phrase above into concrete actions
-        items = aPerson.shoppingCart.getCartItems();
-          ok = false;
-          if ((items[0].product === productToAdd1 && items[0].quantity === CaptianQty) &&
-            (items[1].product === productToAdd2 && items[1].quantity ===CanellaQty) &&
-            (items[2].product === productToAdd3 && items[2].quantity === PurcariQty))
 
-          {
-            ok = true;
-           
-          }
-        console.warn(items); 
-        assert(ok, "Didn't find the added 3 bottles of Captain Morgan, 2 bottels of Canella and 1 bottel of Purcari in the shopping cart."); 
-        callback();
-
-      });
+  this.Then(/^(\d+) bottles of Renat, (\d+) bottles of Canella and (\d+) bottle of Purcari beverages should be add to his\/her shopping cart$/, function(CaptianQty, CanellaQty, PurcariQty, callback) {
 
 
-    // SCENARIO ---- 3
+    // Write code here that turns the phrase above into concrete actions
+    items = aPerson.shoppingCart.getCartItems();
+    ok = false;
+    if ((items[0].product === productToAdd1 && items[0].quantity == CaptianQty) &&
+      (items[1].product === productToAdd2 && items[1].quantity == CanellaQty) &&
+      (items[2].product === productToAdd3 && items[2].quantity == PurcariQty))
 
-    this.Given(/^the user is in online web shop$/, function(callback) {
-      // Need to write code for frontend test
-      callback();
-    });
+    {
+      ok = true;
+
+    }
+    //console.warn("items are" , items);
+    assert(ok, "Didn't find the added 3 bottles of Renat, 2 bottels of Canella and 1 bottel of Purcari in the shopping cart.");
+    callback();
+
+  });
 
 
-    this.When(/^the user is trying to add (\d+) bottles of Captain Morgan to his\/her shopping cart$/, function(CaptianQty, callback) {
+  // SCENARIO ---- 3
 
-      // The user is adding 100 botels of Captian Morgan
-      for (let product of myApp.products) {
-        if (product.namn === "Captain Morgan") {
-          productToAdd = product;
-          break;
-        }
+  this.Given(/^the user is in online web shop$/, function(callback) {
+    // Need to write code for frontend test
+
+    callback();
+  });
+
+
+  this.When(/^the user is trying to add (\d+) bottles of Captain Morgan to his\/her shopping cart$/, function(CaptianQty, callback) {
+
+    // if there is any items in the cart remove all
+    aPerson.shoppingCart.removeAllItems();
+
+    // The user is adding 100 botels of Captian Morgan
+    for (let product of myApp.products) {
+      if (product.namn === "Captain Morgan") {
+        productToAdd = product;
+        break;
       }
-      // it should be deafaultly add 99 bottels
-      //if(product.quantity >= 100)
-      
-      aPerson.shoppingCart.add(productToAdd, 100) 
-      
-      
-      
-      
+    }
 
-      //console.warn(aPerson.shoppingCart);
-      console.warn('^^^^^^^^', aPerson.shoppingCart.thingsToBuy);
+    aPerson.shoppingCart.add(productToAdd, CaptianQty);
 
-      callback();
-    });
+    callback();
+  });
 
-    this.Then(/^the system should be display an error message as (\d+) is maximum quantity than can be ordered$/, function(arg1, callback) {
-      // Write code here that turns the phrase above into concrete actions
-      callback();
-    });
+  this.Then(/^the system should be display an error message as (\d+) is maximum quantity than can be ordered$/, function(CaptianQty, callback) {
 
-    this.Then(/^the sytem should be added (\d+) bottels of Captain Morgan defaultly to his\/her shopping cart$/, function(arg1, callback) {
-      // The sytem should be added 99 bottels of Captain Morgan to his/her cart
+    assert(CaptianQty, " You can't add  :");
 
-      callback();
-    });
+    callback();
+  });
+
+  this.Then(/^the sytem should be added (\d+) bottels of Captain Morgan defaultly to his\/her shopping cart$/, function(CaptianQty, callback) {
+
+    // The sytem should be added 99 bottels of Captain Morgan to his/her cart
+
+    assert(aPerson.shoppingCart.thingsToBuy[0].quantity == CaptianQty, "--------99 bottels are added")
+
+    callback();
+  });
 
 
-    // SCENARIO 4
+  // SCENARIO 4
+
+  //Then the system should be display an error message as 99 is maximum quantity than can be ordered
 
 
-    this.Given(/^the user's shopping cart has already added (\d+) bottles of Motzenbäcker Marie to his\/her cart$/, function(MotzenbäckerQty, callback) {
-      // Write code here that turns the phrase above into concrete actions
-      for (let product of myApp.products) {
-      if (product.namn === "Motzenbäcker") 
-      {
+  this.Given(/^the user's shopping cart has already added (\d+) bottles of Motzenbäcker Marie to his\/her cart$/, function(MotzenbäckerQty, callback) {
+    // Write code here that turns the phrase above into concrete actions
+
+    aPerson.shoppingCart.removeAllItems();
+
+    for (let product of myApp.products) {
+      if (product.namn === "Motzenbäcker") {
         productToAdd = product;
         break;
       }
     }
     aPerson.shoppingCart.add(productToAdd, MotzenbäckerQty);
-      callback();
-    });
+    callback();
+  });
 
-    this.When(/^the user is trying to add (\d+) more bottle of Captain Morgan to his\/her shopping cart$/, function(arg1, callback) {
-      // Write code here that turns the phrase above into concrete actions
-      callback();
-    });
+  this.When(/^the user is trying to add (\d+) more bottle of Captain Morgan to his\/her shopping cart$/, function(CaptianQty, callback) {
+    // Write code here that turns the phrase above into concrete actions
 
-    
+    for (let product of myApp.products) {
+      if (product.namn === "Captain Morgan") {
+        productToAdd = product;
+        break;
+      }
+    }
+    aPerson.shoppingCart.add(productToAdd, CaptianQty);
 
-    // SCENARIO 5
-
-    this.Given(/^the user has already added (\d+) bottles of Captain Morgan to his\/her shopping cart$/, function(arg1, callback) {
-      // Write code here that turns the phrase above into concrete actions
-      callback();
-    });
+    assert(aPerson.shoppingCart.thingsToBuy[0].quantity == CaptianQty, "****  Wrong quantity is adding and max is 99")
+    callback();
 
 
-    this.Then(/^the total of (\d+) bottles of Captain Morgan should be added to his\/her shopping cart$/, function(arg1, callback) {
-      // Write code here that turns the phrase above into concrete actions
-      callback();
-    });
+  });
 
 
 
-  }
+  // SCENARIO 5
+
+
+  this.Given(/^the user has already added (\d+) bottles of Amicone to his\/her shopping cart$/, function(AmicanQty, callback) {
+    // Adding 3 bottels of Amicone
+     aPerson.shoppingCart.removeAllItems();
+     // console.warn("ÄÄÄÄÄÄÄÄÄÄ", aPerson.shoppingCart.thingsToBuy);
+
+
+    for (let product of myApp.products) {
+      if (product.namn === "Amicone") {
+        productToAdd2 = product;
+        break;
+      }
+    }
+    aPerson.shoppingCart.add(productToAdd2, AmicanQty);
+
+    callback();
+  });
+
+
+  this.When(/^the user is trying to add (\d+) more bottle of Amicone to his\/her shopping cart$/, function(AmicanQty, callback) {
+    // Write code here that turns the phrase above into concrete actions
+
+    for (let product of myApp.products) {
+      if (product.namn === "Amicone") {
+        productToAdd2 = product;
+        break;
+      }
+    }
+
+    aPerson.shoppingCart.add(productToAdd2, AmicanQty);
+    callback();
+  });
+
+  this.Then(/^the total of (\d+) bottles of Amicone should be added to his\/her shopping cart$/, function(AmicanQty, callback) 
+  {
+    //assert(aPerson.shoppingCart.thingsToBuy[0].quantity == AmicanQty, "wrong qua...")
+    //console.warn("_________________", aPerson.shoppingCart.thingsToBuy);
+   
+    callback();
+  });
+
+
+
+
+}
