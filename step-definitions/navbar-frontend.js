a75a50363c87abd2008df1dfa52dda1ceaa9265d
Jesper <Mollezeta@gmail.com>
2018-05-18 15:24:10 +0200

    Navbar tests PPE with mattis o jeppe
    Tests the links on the pages

--- step-definitions/navbar-frontend.js
+++ step-definitions/navbar-frontend.js
@@ -4,35 +4,47 @@ module.exports = function() {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
 
+  let testPage;
+
   this.Given(/^that the user is on the home page$/, async function () {
-    await helpers.loadPage('http://localhost:3000');
-    await sleep(2000);
+    await helpers.loadPage('http://localhost:3000/index.html');
   
   });
 
   this.When(/^the user clicks on the "([^"]*)" link$/, async function (arg1) {
     
-    if (arg1 == "home") {
-      let homePage = await driver.findElement(by.css('#index'));
-      await homePage.click();
+    if (arg1 == "index") {
+      testPage = await driver.findElement(by.css('#index'));
+      await testPage.click();
       
     }
 
     if (arg1 == "sortiment") {
-      let homePage = await driver.findElement(by.css('#sortiment'));
-      await homePage.click();
+      testPage = await driver.findElement(by.css('#sortiment'));
+      await testPage.click();
       
     }
 
-    if (arg1 == "Shopping cart") {
-      let homePage = await driver.findElement(by.css('#shoppingcart'));
-      await homePage.click();
-      
+    if (arg1 == "shoppingcart") {
+      testPage = await driver.findElement(by.css('#shoppingcart'));
+      await testPage.click();
+
     }
   });
 
   this.Then(/^the user redirects to the "([^"]*)" page$/, async function (arg1) {
-    await sleep(5000);
+    let titleCheck = await driver.getTitle();
+    let pageCheck = arg1;
+    assert(pageCheck == titleCheck, "The redirect didn't work, you got redirected to the page with a title of " + titleCheck);
+ });
+
+  this.Given(/^that the user is on the sortiment page$/, async function () {
+    await helpers.loadPage('http://localhost:3000/sortiment.html');
+ 
   });
 
+  this.Given(/^that the user is on the shoppingcart page$/, async function () {
+    await helpers.loadPage('http://localhost:3000/shoppingcart.html');
+ 
+  });
 }
