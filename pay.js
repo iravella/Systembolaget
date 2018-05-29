43906999a5bb5f6c2e8f6bb796b9ebbde41c2c89
Unknown <tryhardmatte@gmail.com>
2018-05-29 12:18:39 +0200

    PP matte jesper joel. Added started with payment html

---
+++ pay.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+	console.log("ready");
+
+
+	$("#pay").click(function(){
+		  alert("Thank you for your order!");
+	// $("#warning").remove("#warning");
+	 //($(".firstNm").append("<p id='warning'> Your name is wrong try to retype</p>");
+	});
+
+
+ });
+// module.exports =class Pay{
+// 	constructor(){
+// 		this.payment();
+// 	}
+// 	payment(){
+// 		$(document).ready(function(){
+// 			$("#pay").click(function(){
+//  				alert("Thank you for your order!");
+// 			});
+// 		});
+// 	}
+// }	
+
+
+
+
+
+
+
+
+
+	// $(".firstNm").append("<p id='warning'> Your name is wrong try to retype</p>")
