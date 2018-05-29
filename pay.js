256e6fb2c0476df14755a4c9eb6f4c7cea37ea46
Unknown <tryhardmatte@gmail.com>
2018-05-29 15:40:04 +0200

    Pear programming Jesper Mattias. PAYMENT IS WORKING TRY IT OUT!

--- pay.js
+++ pay.js
@@ -2,14 +2,95 @@ $(document).ready(function() {
 	console.log("ready");
 
 
+	let first = false;
+	let last = false;
+	let cardn = false;
+	let cvcn= false;
+
+		//adding the total sum
+		//ifall vi hinner
+	$(".price").append("<p id='sum'> Totalsumma: " + ShoppingCart.sum + "</p>");
+
+
 	$("#pay").click(function(){
-		  alert("Thank you for your order!");
-	// $("#warning").remove("#warning");
-	 //($(".firstNm").append("<p id='warning'> Your name is wrong try to retype</p>");
+		 let fName = $("#firstName").val();
+		 let lName = $("#lastName").val();
+		 let cNumber = $("#cardNumber").val();
+		 let verNumber = $("#verificationNumber").val();
+	
+	 
+		 
+
+
+	 if(!isNaN(fName)|| fName === ""){
+			$("#Fwarning").remove("#Fwarning");
+	 		$(".firstNm").append("<p id='Fwarning'> Ditt namn innehåller ej giltiga krusiduller</p>");
+	 		first = false;
+		} else {
+			$("#Fwarning").remove("#Fwarning");
+			first = true;
+			
+		}
+
+	 if(!isNaN(lName)|| lName === ""){
+			$("#Lwarning").remove("#Lwarning");
+	 		$(".lastNm").append("<p id='Lwarning'> Ditt Efternamn innehåller ej giltiga krusiduller</p>");
+	 		last = false;
+		} else {
+			$("#Lwarning").remove("#Lwarning");
+			last = true;
+		}
+
+	  if(isNaN(cNumber)|| cNumber === "" || cNumber.length != 16){
+			$("#Cardwarning").remove("#Cardwarning");
+	 		$(".cardNb").append("<p id='Cardwarning'> Var vänlig att kolla på ditt kort en gång till måste vara 16 nummer</p>");
+	 		cardn = false;
+		} else {
+			$("#Cardwarning").remove("#Cardwarning");
+			cardn = true;
+		}
+
+	  if(isNaN(verNumber)|| verNumber === "" || verNumber.length != 3){
+			$("#verwarning").remove("#verwarning");
+	 		$(".cvc").append("<p id='verwarning'> Kolla på baksidan av ditt kort på Cvc måste vara 3 nummer</p>");
+	 		cvcn = false;
+		} else {
+			$("#verwarning").remove("#verwarning");
+			cvcn = true;
+		}
+
+
+		if(first == true && last == true && cardn == true && cvcn == true){
+		 alert("Tack för att du handla hos oss! Din order är påväg.");
+		 // to remove all items
+		 //ShoppingCart.removeAllItems();
+		 document.location.href = "https://www.youtube.com/watch?v=lXMskKTw3Bc";
+		 //document.location.href = "http://localhost:3000/index.html";
+		}
 	});
 
 
  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // module.exports =class Pay{
 // 	constructor(){
 // 		this.payment();
