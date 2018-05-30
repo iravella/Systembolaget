056de82ff7ecd60cb6c2b61d2b67419b507bff47
Unknown <tryhardmatte@gmail.com>
2018-05-30 16:59:09 +0200

    PPE Mattias, JOEL och jesper Paymenttest done

--- features/payment-frontend.feature
+++ features/payment-frontend.feature
@@ -3,15 +3,13 @@ Feature:
 
     Scenario: As a user I would like to get a confirmation when I enter my payment information
         Given that I have chosen to pay for products in my shopping cart
-        When I enter my payment information 
-        And my payment inputs are valid
+        When I enter valid payment information
         And press the payment button
         Then I should recieve a verification of my payment 
 
   	Scenario: As a user I want to know if my payment inputs are invalid so that I can correct them
         Given that I have chosen to pay for products in my shopping cart
-        When I enter my payment information 
-        And my payment inputs are invalid
+        When I enter invalid payment information
         Then I should recieve information about what inputs are invalid
 
     Scenario: As a user i would like to go to the home page when i payed for my products
--- step-definitions/payment-frontend.js
+++ step-definitions/payment-frontend.js
@@ -11,45 +11,72 @@ module.exports = function() {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
 
-  this.Given(/^that I have chosen to pay for products in my shopping cart$/, function () {
-         // Write code here that turns the phrase above into concrete actions
+  this.Given(/^that I have chosen to pay for products in my shopping cart$/, async function () {
+         await helpers.loadPage("http://localhost:3000/shoppingcart.html");
+         await $("#payButton").click();
+         await sleep(1000);
+
    
        });
 
 
-       this.When(/^I enter my payment information$/, function () {
-         // Write code here that turns the phrase above into concrete actions
+       this.When(/^I enter valid payment information$/, async function () {
+        let fr = await $("#firstName");
+        await fr.click();
+        await sleep(1000);
+        await fr.sendKeys("Pelle");
+        await sleep(1000);
+
+        let ls = await $("#lastName");
+        await ls.click();
+        await sleep(1000);
+        await ls.sendKeys("Khan");
+        await sleep(1000);
+       
+       });
+
+       this.When(/^press the payment button$/, async function () {
+       let paybutton = await $("#pay");
+        await paybutton.click();
+        await sleep(1000);  
        
        });
 
 
-
-       this.When(/^my payment inputs are valid$/, function () {
-         // Write code here that turns the phrase above into concrete actions
-       
+       this.Then(/^I should recieve a verification of my payment$/, async function () {
+        let alerttext = await driver.switchTo().alert().getText();  
+        assert(alerttext == "Tack för att du handla hos oss! Din order är påväg.", "The alert didnt show!")
+        await driver.switchTo().alert();
        });
 
+       //-------------------SCENARIO 2 --------------------------
 
-       this.When(/^press the payment button$/, function () {
-         // Write code here that turns the phrase above into concrete actions
+       this.When(/^I enter invalid payment information$/, async function () {
+           let fr = await $("#firstName");
+        await fr.click();
+        await sleep(1000);
+        await fr.sendKeys("123");
+        await sleep(1000);
+
+        let ls = await $("#lastName");
+        await ls.click();
+        await sleep(1000);
+        await ls.sendKeys("123");
+        await sleep(1000);
+
+        //trigger pay
+         let paybutton = await $("#pay");
+        await paybutton.click();
+        await sleep(1000);  
        
-       });
-
-
-       this.Then(/^I should recieve a verification of my payment$/, function () {
-         // Write code here that turns the phrase above into concrete actions
       
        });
 
-
-
-       this.When(/^my payment inputs are invalid$/, function () {
-         // Write code here that turns the phrase above into concrete actions
-      
-       });
-
-       this.Then(/^I should recieve information about what inputs are invalid$/, function () {
-         // Write code here that turns the phrase above into concrete actions
+       this.Then(/^I should recieve information about what inputs are invalid$/, async function () {
+         let FirstWarning = await $("#Fwarning").isDisplayed();
+         let LastWarning = await $("#Lwarning").isDisplayed();
+         assert(await FirstWarning == true, "the first warning text is not displayed");
+         assert(await LastWarning == true, "the last warning text is not displayed");
        
        });
 
@@ -63,21 +90,21 @@ module.exports = function() {
        this.Given(/^have typed in the correct information$/, async function () {
         let fr = await $("#firstName");
         await fr.click();
-        await sleep(3000);
+        await sleep(1000);
         await fr.sendKeys("Pelle");
-        await sleep(3000);
+        await sleep(1000);
 
         let ls = await $("#lastName");
         await ls.click();
-        await sleep(3000);
+        await sleep(1000);
         await ls.sendKeys("Khan");
-        await sleep(3000);
+        await sleep(1000);
        });
 
        this.When(/^the user clicks the paybutton$/, async function () {
         let paybutton = await $("#pay");
         await paybutton.click();
-        await sleep(3000);
+        await sleep(1000);
         await driver.switchTo().alert().accept();
 
         //code to accept the alert
@@ -87,5 +114,6 @@ module.exports = function() {
           let titleCheck = await driver.getTitle();
           let pageCheck = "Quality Liquor Store";
           assert(pageCheck == titleCheck, "The redirect didn't work, you got redirected to the page with a title of " + titleCheck);
+          await sleep(2000);
        });
      }
