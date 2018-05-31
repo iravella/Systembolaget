fd42b8ebc89b8d4ed2e4c95a178d229815d94d92
swapna <swapnasudha.gandhi@gmail.com>
2018-05-31 11:06:24 +0200

    countrywise(name,price)frontend

--- step-definitions-frontend/countrywise-frontend.js
+++ step-definitions-frontend/countrywise-frontend.js
@@ -3,7 +3,7 @@
 
 
  this.Given(/^that the user is on systembolaget$/, async function() {
- 	let driver = await helpers.loadPage('http://localhost:3000');
+ 	await helpers.loadPage('http://localhost:3000');
          console.log("page is loaded");
          await sleep(1000);
          
@@ -30,9 +30,43 @@
   // <div class="Australien buttonsOfLandFilter dropdown-item" style="">Australien&nbsp;</div>
 
    this.Then(/^the lists of products from that country should be displayed in increasing order$/,async function() {
-    
+      
+      
          
        });
+
+
+       // /scenaro 2
+    this.Given(/^that the user is on the sytembolaget$/, async function () {
+      await helpers.loadPage('http://localhost:3000');
+         console.log("page is loaded");
+         await sleep(1000);
+
+    });  
+
+     this.When(/^the user chooses the name in the sorting and the country name Portugal$/, async function () {
+
+      let sortiment = await $('#Sortiment');
+     await sortiment.click();
+         await sleep(1000);
+         let sortpris = await $('.sorteraNamn');
+         await sortpris.click();
+         await sleep(1000);
+         let country = await $('#mainfiltermenu4');
+         await country.click();
+         let countryname = await $('.Portugal.buttonsOfLandFilter.dropdown-item');
+         await sleep(1000);
+         await countryname.click();
+         await sleep(10000);
+         
+        
+       });
+
+
+       this.Then(/^the list of products from that country should be displayed in alphabetical order$/, async function () {
+         
+       });
+
 
     
 }
