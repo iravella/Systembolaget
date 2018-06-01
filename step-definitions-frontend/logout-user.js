61acdc22724672750290fc55088b692f2b7e7326
Unknown <ajay.ravella@gmail.com>
2018-06-01 09:39:19 +0200

    minor fix AJ, Sandhya, Swapna

--- step-definitions-frontend/logout-user.js
+++ step-definitions-frontend/logout-user.js
@@ -1,20 +1,38 @@
+var { $, sleep } = require('./funcs');
 module.exports = function() {
 
-	function sleep(ms) {
-    	return new Promise(resolve => setTimeout(resolve, ms));
-  	}
 
-   	this.When(/^the user clicks on logout button$/, async function () {
-   		await sleep(3000);
-   		let logOut = await driver.findElement(by.css(".loginItem3"));
-   		await logOut.click();
-  	});
+    this.Given(/^that the user is logged in on the home page$/, async function() {
+        await helpers.loadPage('http://localhost:3000');
+        await sleep(10000);
 
-   this.Then(/^the user should be logout from the page$/, async function () {
-    	await sleep(1000);
+        // Enter the username in the username feild
+        let username = await $('.login');
+        await username.click();
+        await username.sendKeys('Tobias');
 
-    	let isLoggedIn = await driver.findElements(by.css(".loggedIn"));
- 		
-    	assert(isLoggedIn.length == 0, "The user is still logged in");
-   	});
+        // Enter the password in the password feild
+        let userPassword = await $('.password');
+        await userPassword.click();
+        await userPassword.sendKeys("4321");
+
+        let clikBtn = await $('.loginItem3');
+    await clikBtn.click();
+    await sleep(8000);
+
+    });
+
+    this.When(/^the user clicks on logout button$/, async function() {
+        await sleep(3000);
+        let logOut = await driver.findElement(by.css(".loginItem3"));
+        await logOut.click();
+    });
+
+    this.Then(/^the user should be logout from the page$/, async function() {
+        await sleep(1000);
+
+        let isLoggedIn = await driver.findElements(by.css(".loggedIn"));
+
+        assert(isLoggedIn.length == 0, "The user is still logged in");
+    });
 }
